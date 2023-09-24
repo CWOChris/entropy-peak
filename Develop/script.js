56e195c4993038a6f3cf509532712c0e98624b73
Chris R <136107779+CWOChris@users.noreply.github.com>
2023-09-24 05:51:49 -0500

    add the strings for the 3 types of passwords

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,45 @@
 // Assignment code here
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHJKLMNOPQRSTUVWXYZ";
+var arabic = "0123456789";
+var specChar = "!@#$%^&*()_+-=";
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var zero = lowercase + uppercase + arabic + specChar;
+var two = lowercase + uppercase + arabic;
+var three = lowercase + uppercase;
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // FROM STARTER CODE NEXT 1 LINE = Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // FROM STARTER CODE NEXT 7 LINES = Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // FROM STARTER CODE NEXT 1 LINE= Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
